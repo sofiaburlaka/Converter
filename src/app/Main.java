935698e6e7ec669b483cc0d66eb2d30ae19fcba4
Sofia Burlaka <sofiaburlaka@gmail.com>
2024-09-03 21:58:18 +0300

    first version converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,19 @@
 package app;
 
 public class Main {
+
+    private static final double CONV_K = 2.20462;
+
     public static void main(String[] args) {
-        System.out.println("Hello world, friends");
+        System.out.println("Converter app");
+
+        double kgs = 5;
+        double pounds = convKgstoPounds(kgs);
+        System.out.println("Result is" + pounds + "pounds");
     }
+
+    private static double convKgstoPounds(double kgs) {
+        return kgs * CONV_K;
+    }
+
 }
