b84099232eb922e08874c9a76724c016a975ab2b
Sofia Burlaka <sofiaburlaka@gmail.com>
2024-09-03 22:24:29 +0300

    changes in converter

--- src/app/Main.java
+++ src/app/Main.java
@@ -8,12 +8,17 @@ public class Main {
         System.out.println("Converter app");
 
         double kgs = 5;
+        double pnds = 12;
         double pounds = convKgstoPounds(kgs);
-        System.out.println("Result is" + pounds + "pounds");
+        double kilos = convPoundstoKgs(pnds);
+        System.out.println("Result is" + pounds + "pounds and " + kilos + "kilograms");
     }
 
     private static double convKgstoPounds(double kgs) {
         return kgs * CONV_K;
+    }
+    private static double convPoundstoKgs(double pnds) {
+        return pnds / CONV_K;
     }
 
 }
